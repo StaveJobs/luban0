cdb9427c0c45b8f483439df65b6a1e4cd9d0f0f2
StaveJobs <kuailenanhai88@126.com>
2019-09-30 14:44:53 +0800

    add commit for main().

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,10 @@
  * 1.0
  */
 public class HelloWorld {
+    /**
+     * 这是我的小小注释...
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
     }
